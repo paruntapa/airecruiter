f3b313c9a4f0c22f8fb6fa002a402e1a3cd00371
paruntapa <geniustechmax@gmail.com>
2025-09-08 15:45:10 +0530

    redirecting to dashboard

--- app/page.js
+++ app/page.js
@@ -1,11 +1,27 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useRouter } from "next/navigation"
 
 export default function Home() {
-  return (
-    <div>
-      zac tel
-      <Button>Click here</Button>
-    </div>
-  );
+
+	const router = useRouter();
+
+	const handleClick = () => {
+		router.push("/dashboard")
+
+	};
+    return (
+      <div className="flex justify-center w-screen items-center h-screen">
+	  <div className="flex flex-col items-center gap-5">	  
+	    <div>
+              Created by - Tel Zac 
+	    </div>
+            <Button onClick={handleClick}>
+	     Go to Dashboard 
+	    </Button>
+          </div>
+     </div>
+    );
 }
